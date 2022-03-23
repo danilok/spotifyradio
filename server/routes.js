266a2ad0cad7d3c9183c4af6e38c35a5f662dbc4
Danilo Yorinori <daniloksy@gmail.com>
2022-03-22 22:04:15 -0300

    Segundo dia

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,7 @@
 import config from './config.js'
 import { Controller } from './controller.js'
 import { logger } from './util.js'
-
+import { once } from 'events'
 const {
   location,
   pages: {
@@ -47,6 +47,24 @@ async function routes(request, response) {
     //   'Content-Type': 'text/html'
     // })
     return stream.pipe(response)
+  }
+
+  if (method === 'GET' && url.includes('/stream')) {
+    const { stream, onClose } = controller.createClientStream()
+    request.once("close", onClose)
+    response.writeHead('200', {
+      'Content-Type': 'audio/mpeg',
+      'Accept-Rages': 'bytes'
+    })
+
+    return stream.pipe(response)
+  }
+
+  if (method === 'POST' && url === '/controller') {
+    const data = await once(request, 'data')
+    const item = JSON.parse(data)
+    const result = await controller.handleCommand(item)
+    return response.end(JSON.stringify(result))
   }
 
   // files
